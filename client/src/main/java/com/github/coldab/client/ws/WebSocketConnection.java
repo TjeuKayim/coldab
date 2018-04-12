7a7f599b863c40921e9dcda8d4ffeed6452e3cb7
Tjeu Kayim <tjeu.kayim@gmail.com>
2018-04-12 15:27:50 +0200

    Flatten class and make use of MessageEncoder

--- client/src/main/java/com/github/coldab/client/ws/WebSocketConnection.java
+++ client/src/main/java/com/github/coldab/client/ws/WebSocketConnection.java
@@ -1,5 +1,13 @@
 package com.github.coldab.client.ws;
 
+import com.github.coldab.shared.ws.ClientEndpoint;
+import com.github.coldab.shared.ws.MessageEncoder;
+import com.github.coldab.shared.ws.ServerEndpoint;
+import com.github.tjeukayim.socketinterface.SocketMessage;
+import com.github.tjeukayim.socketinterface.SocketReceiver;
+import com.github.tjeukayim.socketinterface.SocketSender;
+import java.io.IOException;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.WebSocketSession;
@@ -8,30 +16,49 @@ import org.springframework.web.socket.client.WebSocketConnectionManager;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
-public class WebSocketConnection {
+public class WebSocketConnection extends TextWebSocketHandler {
 
   public static void main(String[] args) throws Exception {
-    WebSocketClient client = new StandardWebSocketClient();
-    WebSocketHandler handler = new SocketHandler();
-    String url = "ws://localhost:8080/ws";
-    WebSocketConnectionManager manager = new WebSocketConnectionManager(client, handler, url);
-    manager.start();
+    new WebSocketConnection("ws://localhost:8080/ws");
     Thread.sleep(3000);
   }
 
-  public static class SocketHandler extends TextWebSocketHandler {
+  private WebSocketSession session;
+  private SocketReceiver socketReceiver;
 
-    private WebSocketSession session;
+  public WebSocketConnection(String url) {
+    WebSocketClient client = new StandardWebSocketClient();
+    WebSocketConnectionManager manager = new WebSocketConnectionManager(client, this, url);
+    manager.start();
+  }
 
-    @Override
-    public void handleTextMessage(WebSocketSession session, TextMessage message) {
-      System.out.println(message.getPayload());
-    }
+  @Override
+  public void afterConnectionEstablished(WebSocketSession session) {
+    this.session = session;
+    System.out.println("Connected WebSocket");
+    this.session = session;
+    ClientEndpoint clientEndpoint = SocketSender.create(ClientEndpoint.class, this::sendMessage);
+    ServerEndpoint serverEndpoint = null;
+    socketReceiver = new SocketReceiver(ServerEndpoint.class, serverEndpoint);
+  }
 
-    @Override
-    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-      this.session = session;
-      session.sendMessage(new TextMessage("Hello World"));
+  @Override
+  public void handleTextMessage(WebSocketSession session, TextMessage message) {
+    byte[] bytes = message.asBytes();
+    MessageEncoder.decodeMessage(bytes, socketReceiver);
+  }
+
+  @Override
+  public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
+
+  }
+
+  private void sendMessage(SocketMessage socketMessage) {
+    byte[] payload = MessageEncoder.encodeMessage(socketMessage);
+    try {
+      session.sendMessage(new TextMessage(payload));
+    } catch (IOException e) {
+      e.printStackTrace();
     }
   }
 }
