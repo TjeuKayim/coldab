d494b37f7938730e845f338a82c09cf27b528f56
Tjeu Kayim <tjeu.kayim@gmail.com>
2018-04-12 11:30:00 +0200

    SocketHandler uses MessageEncoder

--- server/src/main/java/com/github/coldab/server/ws/SocketHandler.java
+++ server/src/main/java/com/github/coldab/server/ws/SocketHandler.java
@@ -1,5 +1,11 @@
 package com.github.coldab.server.ws;
 
+import com.github.coldab.shared.ws.ClientEndpoint;
+import com.github.coldab.shared.ws.MessageEncoder;
+import com.github.coldab.shared.ws.ServerEndpoint;
+import com.github.tjeukayim.socketinterface.SocketMessage;
+import com.github.tjeukayim.socketinterface.SocketReceiver;
+import com.github.tjeukayim.socketinterface.SocketSender;
 import java.io.IOException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
@@ -10,18 +16,31 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 public class SocketHandler extends TextWebSocketHandler {
 
   private WebSocketSession session;
-
-  @Override
-  public void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
-    System.out.println(message.getPayload());
-    session.sendMessage(message);
-  }
-
+  private SocketReceiver socketReceiver;
 
   @Override
   public void afterConnectionEstablished(WebSocketSession session) {
+    // TODO: Connect endpoints
     System.out.println("Connected WebSocket");
     this.session = session;
+    ServerEndpoint serverEndpoint = SocketSender.create(ServerEndpoint.class, this::sendMessage);
+    ClientEndpoint clientEndpoint = null;
+    socketReceiver = new SocketReceiver(ClientEndpoint.class, clientEndpoint);
+  }
+
+  @Override
+  public void handleTextMessage(WebSocketSession session, TextMessage message) {
+    byte[] bytes = message.asBytes();
+    MessageEncoder.decodeMessage(bytes, socketReceiver);
+  }
+
+  private void sendMessage(SocketMessage socketMessage) {
+    byte[] payload = MessageEncoder.encodeMessage(socketMessage);
+    try {
+      session.sendMessage(new TextMessage(payload));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 
 }
