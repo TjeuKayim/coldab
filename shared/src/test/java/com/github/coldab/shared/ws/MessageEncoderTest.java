1c8b782471f15c4fa61ed1bc4facd07ade11eac3
Tjeu Kayim <tjeu.kayim@gmail.com>
2018-04-14 15:24:54 +0200

    Create unit-test for MessageEncoderTest

---
+++ shared/src/test/java/com/github/coldab/shared/ws/MessageEncoderTest.java
@@ -0,0 +1,54 @@
+package com.github.coldab.shared.ws;
+
+import static org.junit.Assert.assertEquals;
+
+import com.github.tjeukayim.socketinterface.SocketMessage;
+import com.github.tjeukayim.socketinterface.SocketReceiver;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MessageEncoderTest {
+
+  private SocketReceiver socketReceiver;
+  private Receiver receiver;
+
+  @Before
+  public void setUp() throws Exception {
+    receiver = new Receiver();
+    socketReceiver = new SocketReceiver(Protocol.class, receiver);
+  }
+
+  @Test
+  public void encodeDecode() {
+    SocketMessage message = new SocketMessage("e", "m", "a1", 2);
+    byte[] bytes = MessageEncoder.encodeMessage(message);
+    String debug = new String(bytes);
+    System.out.println(debug);
+    MessageEncoder.decodeMessage(bytes, socketReceiver);
+    assertEquals("a1", receiver.a1);
+    assertEquals(2, receiver.a2);
+  }
+
+  interface Protocol {
+    E e();
+  }
+
+  interface E {
+    void m(String a1, int a2);
+  }
+
+  class Receiver implements Protocol {
+
+
+    private String a1;
+    private int a2;
+
+    @Override
+    public E e() {
+      return (a1, a2) -> {
+        this.a1 = a1;
+        this.a2 = a2;
+      };
+    }
+  }
+}
