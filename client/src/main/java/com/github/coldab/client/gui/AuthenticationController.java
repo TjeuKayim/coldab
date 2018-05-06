9400fbdcfaacded05936e3a4afab1bce9117d5b1
Tjeu Kayim <tjeu.kayim@gmail.com>
2018-05-06 13:59:55 +0200

    Fix code style using InteliJ's "Reformat code" and "Optimize Imports"

--- client/src/main/java/com/github/coldab/client/gui/AuthenticationController.java
+++ client/src/main/java/com/github/coldab/client/gui/AuthenticationController.java
@@ -1,27 +1,24 @@
 package com.github.coldab.client.gui;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
 public class AuthenticationController implements Initializable {
 
-    @FXML
-    private WebView googleWebview;
+  @FXML
+  private WebView googleWebview;
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        WebEngine browser = googleWebview.getEngine();
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    WebEngine browser = googleWebview.getEngine();
 
-        googleWebview.prefHeight(400);
-        googleWebview.prefWidth(600);
+    googleWebview.prefHeight(400);
+    googleWebview.prefWidth(600);
 
-        browser.load("https://google.nl");
-
-
-    }
+    browser.load("https://google.nl");
+  }
 }
--- client/src/main/java/com/github/coldab/client/gui/ColdabApplication.java
+++ client/src/main/java/com/github/coldab/client/gui/ColdabApplication.java
@@ -15,24 +15,23 @@ public class ColdabApplication extends Application {
 
   @Override
   public void start(Stage primaryStage) throws IOException {
-      this.authenticationStage = primaryStage;
-      FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/authentication.fxml"));
-      loader.setControllerFactory(c -> new AuthenticationController());
-      Parent root = loader.load();
-      authenticationStage.setTitle("Coldab text");
-      Scene scene = new Scene(root);
-      authenticationStage.setScene(scene);
-      authenticationStage.show();
+    this.authenticationStage = primaryStage;
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/authentication.fxml"));
+    loader.setControllerFactory(c -> new AuthenticationController());
+    Parent root = loader.load();
+    authenticationStage.setTitle("Coldab text");
+    Scene scene = new Scene(root);
+    authenticationStage.setScene(scene);
+    authenticationStage.show();
 
-
-//    this.projectChooserStage = primaryStage;
-//    FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/projectChooser.fxml"));
-//    loader.setControllerFactory(c -> new ProjectChooserController(this::startEditor));
-//    Parent root = loader.load();
-//    projectChooserStage.setTitle("Coldab text");
-//    Scene scene = new Scene(root);
-//    projectChooserStage.setScene(scene);
-//    projectChooserStage.show();
+    //    this.projectChooserStage = primaryStage;
+    //    FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/projectChooser.fxml"));
+    //    loader.setControllerFactory(c -> new ProjectChooserController(this::startEditor));
+    //    Parent root = loader.load();
+    //    projectChooserStage.setTitle("Coldab text");
+    //    Scene scene = new Scene(root);
+    //    projectChooserStage.setScene(scene);
+    //    projectChooserStage.show();
   }
 
   private void startEditor(Project project) {
@@ -53,7 +52,7 @@ public class ColdabApplication extends Application {
     stage.setOnCloseRequest(event -> projectChooserStage.show());
   }
 
-  private void startAuthentication(Project project){
+  private void startAuthentication(Project project) {
     projectChooserStage.hide();
     Stage stage = new Stage();
     FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/authentication.fxml"));
