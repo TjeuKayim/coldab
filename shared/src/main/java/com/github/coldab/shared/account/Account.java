9c1de87c12aaf6bc7a7a7d40933530b200608ed0
victorvanleeuwen <v.vn.leeuwen@gmail.com>
2018-05-07 13:43:08 +0200

    changed problems in POM

--- shared/src/main/java/com/github/coldab/shared/account/Account.java
+++ shared/src/main/java/com/github/coldab/shared/account/Account.java
@@ -1,11 +1,25 @@
 package com.github.coldab.shared.account;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Account {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private int id;
   private String nickName;
   private String email;
 
-  public Account() {
+  // this constructor is used by JPA.
+  protected Account() {
+  }
 
+  public Account(String nickName, String email) {
+    this.nickName = nickName;
+    this.email = email;
   }
 }
