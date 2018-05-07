0dbb0f320fac8ac6575a95eb31a55392a894d9c4
victorvanleeuwen <v.vn.leeuwen@gmail.com>
2018-05-07 16:10:46 +0200

    change in annotation and project project: + admins + collaborators annotation: + mentioned

--- shared/src/main/java/com/github/coldab/shared/project/Annotation.java
+++ shared/src/main/java/com/github/coldab/shared/project/Annotation.java
@@ -3,22 +3,32 @@ package com.github.coldab.shared.project;
 import com.github.coldab.shared.account.Account;
 import com.github.coldab.shared.edit.Letter;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class Annotation {
-  private int id;
+
   private final boolean todo;
   private final String text;
   private final Account account;
   private final LocalDateTime creationDate;
   private final Letter start;
 
+
+  private final List<Account> mentioned;
+  private int id;
+
   public Annotation(Account account, LocalDateTime creationDate,
-      Letter start, boolean todo, String text) {
+      Letter start, boolean todo, String text, List<Account> mentioned) {
     this.account = account;
     this.creationDate = creationDate;
     this.start = start;
     this.todo = todo;
     this.text = text;
+    this.mentioned = mentioned;
+  }
+
+  public List<Account> getMentioned() {
+    return mentioned;
   }
 
   public boolean isTodo() {
--- shared/src/main/java/com/github/coldab/shared/project/Project.java
+++ shared/src/main/java/com/github/coldab/shared/project/Project.java
@@ -1,18 +1,32 @@
 package com.github.coldab.shared.project;
 
+import com.github.coldab.shared.account.Account;
 import com.github.coldab.shared.chat.Chat;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class Project {
+
   private int id;
   private String name;
+  private final List<Account> admins = new ArrayList<>();
+  private final List<Account> collaborators = new ArrayList<>();
   private LocalDateTime creationDate;
-  private HashMap<Integer, File> files = new HashMap<>();
+  private  final HashMap<Integer, File> files = new HashMap<>();
   private Chat chat;
 
   public Project(String name) {
     this.name = name;
+  }
+
+  public List<Account> getAdmins() {
+    return admins;
+  }
+
+  public List<Account> getCollaborators() {
+    return collaborators;
   }
 
   public String getName() {
@@ -26,6 +40,7 @@ public class Project {
   public HashMap<Integer, File> getFiles() {
     return files;
   }
+
 
   public int getId() {
     return id;
