4cac3dfc746379e9c197625f7d0a41a12cd270f5
Tjeu Kayim <tjeu.kayim@gmail.com>
2018-05-07 13:57:37 +0200

    Create interface UpdateSender according to "Specificaties van Interfaces"

---
+++ shared/src/main/java/com/github/coldab/shared/ws/UpdateSender.java
@@ -0,0 +1,24 @@
+package com.github.coldab.shared.ws;
+
+import com.github.coldab.shared.edit.Addition;
+import com.github.coldab.shared.edit.Deletion;
+import com.github.coldab.shared.project.Annotation;
+import com.github.coldab.shared.project.BinaryFile;
+import com.github.coldab.shared.project.TextFile;
+import com.github.coldab.shared.session.Caret;
+import java.util.List;
+
+public interface UpdateSender {
+
+  /**
+   * New or updated files.
+   */
+  void files(List<TextFile> textFiles, List<BinaryFile> binaryFiles);
+
+  void removeFile(int fileId);
+
+  void edits(int fileId, List<Addition> additions, List<Deletion> deletions,
+      List<Annotation> annotations);
+
+  void caret(int fileId, Caret caret);
+}
