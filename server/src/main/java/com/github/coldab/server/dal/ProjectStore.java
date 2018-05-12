b29bb52739794e4d09a2e53dc55d5020aeb109cb
Tjeu Kayim <tjeu.kayim@gmail.com>
2018-05-12 13:51:05 +0200

    Create repository where SocketHandler gets it's projects from

--- server/src/main/java/com/github/coldab/server/dal/ProjectStore.java
+++ server/src/main/java/com/github/coldab/server/dal/ProjectStore.java
@@ -1,5 +1,7 @@
 package com.github.coldab.server.dal;
 
-public class ProjectStore {
+import com.github.coldab.shared.project.Project;
+import org.springframework.data.repository.CrudRepository;
 
+public interface ProjectStore extends CrudRepository<Project, Integer>  {
 }
