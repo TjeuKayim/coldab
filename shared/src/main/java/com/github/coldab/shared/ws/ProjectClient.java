5616b643583fc819c69642aef25b21f8f5b57f40
Tjeu Kayim <tjeu.kayim@gmail.com>
2018-05-12 14:32:53 +0200

    Split edits into addition and deletion & Add confirmation messages

--- shared/src/main/java/com/github/coldab/shared/ws/ProjectClient.java
+++ shared/src/main/java/com/github/coldab/shared/ws/ProjectClient.java
@@ -1,10 +1,23 @@
 package com.github.coldab.shared.ws;
 
 import com.github.coldab.shared.account.Account;
+import com.github.coldab.shared.edit.Addition;
+import com.github.coldab.shared.edit.Deletion;
 import java.util.List;
 
 public interface ProjectClient extends UpdateSender {
 
+  void edits(int fileId, List<Addition> additions, List<Deletion> deletions);
+
+  /**
+   * An edit gets send back to the author as confirmation.
+   */
+  void confirmAddition(int fileId, Addition addition);
+
+  /**
+   * An edit gets send back to the author as confirmation.
+   */
+  void confirmDeletion(int fileId, Deletion deletion);
 
   void collaborators(List<Account> admins, List<Account> collaborators);
 }
--- shared/src/main/java/com/github/coldab/shared/ws/UpdateSender.java
+++ shared/src/main/java/com/github/coldab/shared/ws/UpdateSender.java
@@ -1,7 +1,5 @@
 package com.github.coldab.shared.ws;
 
-import com.github.coldab.shared.edit.Addition;
-import com.github.coldab.shared.edit.Deletion;
 import com.github.coldab.shared.project.Annotation;
 import com.github.coldab.shared.project.BinaryFile;
 import com.github.coldab.shared.project.TextFile;
@@ -16,8 +14,6 @@ public interface UpdateSender {
   void files(List<TextFile> textFiles, List<BinaryFile> binaryFiles);
 
   void removeFile(int fileId);
-
-  void edits(int fileId, List<Addition> additions, List<Deletion> deletions);
 
   void annotations(int fileId, List<Annotation> annotations);
 
