81cdf2c8c2368c3b89049b8250166131574ae028
Tjeu Kayim <tjeu.kayim@gmail.com>
2018-05-13 11:22:33 +0200

    Replace MappedSuperclass by Edit

--- shared/src/main/java/com/github/coldab/shared/edit/Edit.java
+++ shared/src/main/java/com/github/coldab/shared/edit/Edit.java
@@ -4,15 +4,15 @@ import com.github.coldab.shared.account.Account;
 import java.time.LocalDateTime;
 import java.util.List;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
 
 /**
  * An Edit is a change in a {@link com.github.coldab.shared.project.TextFile}.
  */
-@MappedSuperclass
+@Entity
 public abstract class Edit {
 
   @Id
