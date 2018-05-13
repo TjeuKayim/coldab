4c862057ff916b7793c8ab698bdc3ffb8e304e86
Tjeu Kayim <tjeu.kayim@gmail.com>
2018-05-13 10:51:46 +0200

    Add EntityScan to find entities in shared package

--- server/src/main/java/com/github/coldab/server/Main.java
+++ server/src/main/java/com/github/coldab/server/Main.java
@@ -5,9 +5,11 @@ import com.github.coldab.shared.project.Project;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EntityScan("com.github.coldab")
 public class Main {
 
   public static void main(String[] args) {
