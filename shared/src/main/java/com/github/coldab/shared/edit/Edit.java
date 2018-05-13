bfe364d35afd18b6a36b1ec1978661c6c0a16dd5
Tjeu Kayim <tjeu.kayim@gmail.com>
2018-05-13 11:53:50 +0200

    Specify inheritance strategy
    http://www.baeldung.com/hibernate-inheritance

--- shared/src/main/java/com/github/coldab/shared/edit/Edit.java
+++ shared/src/main/java/com/github/coldab/shared/edit/Edit.java
@@ -8,11 +8,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 
 /**
  * An Edit is a change in a {@link com.github.coldab.shared.project.TextFile}.
  */
 @Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public abstract class Edit {
 
   @Id
