b0ab5c4c370437b464594b115769ae9d9c2710f9
Tjeu Kayim <tjeu.kayim@gmail.com>
2018-05-14 15:35:58 +0200

    Create TextFileService

--- server/src/main/java/com/github/coldab/server/ws/ProjectService.java
+++ server/src/main/java/com/github/coldab/server/ws/ProjectService.java
@@ -6,6 +6,7 @@ import com.github.coldab.shared.edit.Deletion;
 import com.github.coldab.shared.edit.Edit;
 import com.github.coldab.shared.project.Annotation;
 import com.github.coldab.shared.project.BinaryFile;
+import com.github.coldab.shared.project.File;
 import com.github.coldab.shared.project.Project;
 import com.github.coldab.shared.project.TextFile;
 import com.github.coldab.shared.session.Caret;
@@ -14,10 +15,14 @@ import com.github.coldab.shared.ws.ProjectServer;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ProjectService {
+
   private final Project project;
   private final List<ClientEndpoint> clients = new ArrayList<>();
   private final ProjectStore store;
@@ -43,6 +48,7 @@ public class ProjectService {
   private class MessageReceiver implements ProjectServer {
 
     private final ClientEndpoint clientEndpoint;
+    private final Map<Integer, TextFileService> textFileServices = new HashMap<>();
 
     public MessageReceiver(ClientEndpoint clientEndpoint) {
       this.clientEndpoint = clientEndpoint;
@@ -111,7 +117,42 @@ public class ProjectService {
 
     @Override
     public void files(List<TextFile> textFiles, List<BinaryFile> binaryFiles) {
+      Stream.concat(textFiles.stream(), binaryFiles.stream())
+          .forEach(this::file);
+    }
 
+    private void file(File file) {
+      if (file.getId() == 0) {
+        // Create new
+        project.getFiles().add(file);
+      } else {
+        // Update
+        project.getFilesById().replace(file.getId(), file);
+      }
+      // Save to db and apply changes
+      project.setFiles(store.save(project).getFiles());
+      File updated = project.getFilesById().get(file.getId());
+      // Notify all clients about it
+      filesUpdated(updated);
+    }
+
+    private void filesUpdated(File... files) {
+      List<TextFile> textFiles = new ArrayList<>();
+      List<BinaryFile> binaryFiles = new ArrayList<>();
+      for (File file : files) {
+        if (file instanceof TextFile) {
+          TextFile textFile = (TextFile) file;
+          textFiles.add(textFile);
+          textFileServices.computeIfAbsent(file.getId(),
+              id -> new TextFileService(textFile));
+        } else if (file instanceof BinaryFile) {
+          binaryFiles.add(((BinaryFile) file));
+        }
+      }
+      // Notify clients
+      for (ClientEndpoint client : clients) {
+        client.project().files(textFiles, binaryFiles);
+      }
     }
 
     @Override
---
+++ server/src/main/java/com/github/coldab/server/ws/TextFileService.java
@@ -0,0 +1,12 @@
+package com.github.coldab.server.ws;
+
+import com.github.coldab.shared.project.TextFile;
+
+public class TextFileService {
+
+  private final TextFile file;
+
+  public TextFileService(TextFile file) {
+    this.file = file;
+  }
+}
