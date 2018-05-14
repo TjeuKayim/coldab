5911e3de0bebd12271371a14c71b83e1ad23eaac
Tjeu Kayim <tjeu.kayim@gmail.com>
2018-05-14 19:47:49 +0200

    Rewrote Letter to contain a Position

--- shared/src/main/java/com/github/coldab/shared/edit/Letter.java
+++ shared/src/main/java/com/github/coldab/shared/edit/Letter.java
@@ -1,55 +1,25 @@
 package com.github.coldab.shared.edit;
 
-import java.util.Objects;
-import javax.persistence.Embeddable;
-import javax.persistence.Transient;
-
-/**
- * In database saved as:
- * - additionId
- * - position
- */
-@Embeddable
 public class Letter {
 
-  @Transient
-  private final Addition addition;
+  private final Position position;
+  private final char character;
 
-  private final int position;
-
-  public Letter(Addition addition, int position) {
-    this.addition = addition;
+  public Letter(Position position, char character) {
     this.position = position;
+    this.character = character;
   }
 
-  @Override
-  public String toString() {
-    return String.format("%d:'%s'", position, getCharacter());
+  public Letter(int additionIndex, int position, char character) {
+    this.position = new Position(additionIndex, position);
+    this.character = character;
   }
 
-  public char getCharacter() {
-    return addition.getCharacter(position);
-  }
-
-  public int getPosition() {
+  public Position getPosition() {
     return position;
   }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    Letter letter = (Letter) o;
-    return getCharacter() == ((Letter) o).getCharacter()
-        && position == letter.position;
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(getCharacter(), position);
+  public char getCharacter() {
+    return character;
   }
 }
