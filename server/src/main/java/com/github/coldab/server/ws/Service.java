9122c3039b884d2343804c11ef869e40643564c8
Tjeu Kayim <tjeu.kayim@gmail.com>
2018-05-21 10:59:25 +0200

    Fix refactor mistake

--- server/src/main/java/com/github/coldab/server/ws/Service.java
+++ server/src/main/java/com/github/coldab/server/ws/Service.java
@@ -1,7 +1,6 @@
 package com.github.coldab.server.ws;
 
 import com.github.coldab.shared.account.Account;
-import com.github.coldab.shared.ws.ChatClient;
 
 /**
  * A client can connect to a service, and the service returns a server-interface.
@@ -15,7 +14,7 @@ public interface Service<S, C> {
    * Notifies that a new client has connected.
    * @param account The account of the client, used to check permissions.
    */
-  S connect(ChatClient client, Account account);
+  S connect(C client, Account account);
 
   void disconnect(C client);
 }
