41fdd5278c1dc3be8f2cb71fe77c733079bcda32
Tjeu Kayim <tjeu.kayim@gmail.com>
2018-05-21 13:28:00 +0200

    Test encode other classes of domain-model

--- shared/src/test/java/com/github/coldab/shared/ws/ProjectServerTest.java
+++ shared/src/test/java/com/github/coldab/shared/ws/ProjectServerTest.java
@@ -1,5 +1,7 @@
 package com.github.coldab.shared.ws;
 
+import static com.github.coldab.shared.ws.MessageEncoderTest.encodeDecode;
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 import com.github.coldab.shared.TimeProvider;
@@ -7,6 +9,11 @@ import com.github.coldab.shared.account.Account;
 import com.github.coldab.shared.edit.Addition;
 import com.github.coldab.shared.edit.Deletion;
 import com.github.coldab.shared.edit.Position;
+import com.github.coldab.shared.project.Annotation;
+import com.github.coldab.shared.project.BinaryFile;
+import com.github.coldab.shared.project.TextFile;
+import com.github.coldab.shared.session.Caret;
+import java.util.Collections;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -23,7 +30,7 @@ public class ProjectServerTest {
   public void encodeAddition() {
     Addition expected =
         new Addition(3, account, new Position(4, 5), "Hello World");
-    Addition actual = MessageEncoderTest.encodeDecode(expected);
+    Addition actual = encodeDecode(expected);
     assertEquals(expected, actual);
     assertEquals(expected.getLetters(), actual.getLetters());
   }
@@ -33,7 +40,49 @@ public class ProjectServerTest {
     Deletion expected = new Deletion(3, account,
         new Position(4, 5),
         new Position(6, 7));
-    Deletion actual = MessageEncoderTest.encodeDecode(expected);
+    Deletion actual = encodeDecode(expected);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void encodeBinaryFile() {
+    BinaryFile expected = new BinaryFile(1, "/path/to/hello.world", "hash1234");
+    BinaryFile actual = encodeDecode(expected);
+    assertEquals(expected.getId(), actual.getId());
+    assertEquals(expected.getHash(), actual.getHash());
+    assertArrayEquals(expected.getPath(), actual.getPath());
+  }
+
+  @Test
+  public void encodeTextFile() {
+    TextFile expected = new TextFile(1, "/path/to/hello.world");
+    expected.addEdit(new Addition(0, account, new Position(4, 5), "Hello World"));
+    expected.getAnnotations().add(new Annotation(account, new Position(1, 2), true, "Hello World"));
+
+    TextFile actual = encodeDecode(expected);
+    assertEquals(expected.getId(), actual.getId());
+    assertArrayEquals(expected.getPath(), actual.getPath());
+    // Edits and annotations should not be serialized
+    assertEquals(Collections.emptyList(), actual.getEdits());
+    assertEquals(Collections.emptyList(), actual.getAnnotations());
+  }
+
+  @Test
+  public void encodeAnnotation() {
+    Annotation expected = new Annotation(account, new Position(1, 2), true, "Hello World");
+    Annotation actual = encodeDecode(expected);
+    assertEquals(expected.getAccount(), actual.getAccount());
+    assertEquals(expected.getCreationDate(), actual.getCreationDate());
+    assertEquals(expected.getId(), actual.getId());
+    assertEquals(expected.getStart(), actual.getStart());
+    assertEquals(expected.getText(), actual.getText());
+    assertEquals(expected.isTodo(), actual.isTodo());
+  }
+
+  @Test
+  public void encodeCaret() {
+    Caret expected = new Caret(account, new Position(1, 2));
+    Caret actual = encodeDecode(expected);
     assertEquals(expected, actual);
   }
 }
