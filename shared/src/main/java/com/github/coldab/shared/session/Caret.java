6d590975994f30ac84f401f5257bdee9483d1297
Tjeu Kayim <tjeu.kayim@gmail.com>
2018-05-21 13:05:18 +0200

    Add fields to Caret

--- shared/src/main/java/com/github/coldab/shared/session/Caret.java
+++ shared/src/main/java/com/github/coldab/shared/session/Caret.java
@@ -1,5 +1,23 @@
 package com.github.coldab.shared.session;
 
+import com.github.coldab.shared.account.Account;
+import com.github.coldab.shared.edit.Position;
+
 public class Caret {
 
+  private final Account account;
+  private final Position position;
+
+  public Caret(Account account, Position position) {
+    this.account = account;
+    this.position = position;
+  }
+
+  public Account getAccount() {
+    return account;
+  }
+
+  public Position getPosition() {
+    return position;
+  }
 }
