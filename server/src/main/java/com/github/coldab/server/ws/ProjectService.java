fc48c469d4ade7c440c630bd397f6aefd68c747b
Tjeu Kayim <tjeu.kayim@gmail.com>
2018-05-21 11:00:32 +0200

    Use Subscriptions and Service<> to manage TextFileServices

--- server/src/main/java/com/github/coldab/server/ws/ProjectService.java
+++ server/src/main/java/com/github/coldab/server/ws/ProjectService.java
@@ -1,6 +1,7 @@
 package com.github.coldab.server.ws;
 
 import com.github.coldab.server.dal.ProjectStore;
+import com.github.coldab.shared.account.Account;
 import com.github.coldab.shared.edit.Addition;
 import com.github.coldab.shared.edit.Deletion;
 import com.github.coldab.shared.project.Annotation;
@@ -9,8 +10,10 @@ import com.github.coldab.shared.project.File;
 import com.github.coldab.shared.project.Project;
 import com.github.coldab.shared.project.TextFile;
 import com.github.coldab.shared.session.Caret;
-import com.github.coldab.shared.ws.ClientEndpoint;
+import com.github.coldab.shared.ws.ProjectClient;
 import com.github.coldab.shared.ws.ProjectServer;
+import com.github.coldab.shared.ws.TextFileClient;
+import com.github.coldab.shared.ws.TextFileServer;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -18,10 +21,14 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
 
-public class ProjectService {
+/**
+ * Each project is managed by a ProjectService.
+ */
+public class ProjectService implements Service<ProjectServer, ProjectClient> {
 
   private final Project project;
-  private final List<ClientEndpoint> clients = new ArrayList<>();
+  private final List<ProjectClient> clients = new ArrayList<>();
+  private final Map<Integer, TextFileService> textFileServices = new HashMap<>();
   private final ProjectStore store;
 
   ProjectService(Project project, ProjectStore store) {
@@ -29,36 +36,45 @@ public class ProjectService {
     this.store = store;
   }
 
-  public ProjectServer addClient(ClientEndpoint clientEndpoint) {
-    clients.add(clientEndpoint);
-    return new MessageReceiver(clientEndpoint);
+  @Override
+  public ProjectServer connect(ProjectClient projectClient, Account account) {
+    clients.add(projectClient);
+    return new MessageReceiver(projectClient, account);
   }
 
-  public void removeClient(ClientEndpoint clientEndpoint) {
-    clients.remove(clientEndpoint);
+  @Override
+  public void disconnect(ProjectClient projectClient) {
+    clients.remove(projectClient);
   }
 
-  public List<ClientEndpoint> getClients() {
+  public List<ProjectClient> getClients() {
     return Collections.unmodifiableList(clients);
   }
 
   private class MessageReceiver implements ProjectServer {
 
-    private final ClientEndpoint clientEndpoint;
-    private final Map<Integer, TextFileService> textFileServices = new HashMap<>();
+    private final ProjectClient client;
+    private final Account account;
+    private final Map<Integer, Subscription> subscriptions = new HashMap<>();
 
-    public MessageReceiver(ClientEndpoint clientEndpoint) {
-      this.clientEndpoint = clientEndpoint;
+    public MessageReceiver(ProjectClient client, Account account) {
+      this.client = client;
+      this.account = account;
     }
 
     @Override
     public void subscribe(int fileId) {
-
+      TextFileForwarder forwarder = new TextFileForwarder(client, fileId);
+      TextFileService textFileService = textFileServices.get(fileId);
+      TextFileServer server =
+          textFileService.connect(forwarder, account);
+      subscriptions.put(fileId, new Subscription(server, forwarder));
     }
 
     @Override
     public void unsubscribe(int fileId) {
-
+      Subscription subscription = subscriptions.remove(fileId);
+      textFileServices.get(fileId).disconnect(subscription.textFileClient);
     }
 
     @Override
@@ -83,18 +99,22 @@ public class ProjectService {
 
     @Override
     public void addition(int fileId, Addition addition) {
-      textFileServices.get(fileId).processEdit(addition);
+      textFileClient(fileId).newEdit(addition);
     }
 
     @Override
     public void deletion(int fileId, Deletion deletion) {
-      textFileServices.get(fileId).processEdit(deletion);
+      textFileClient(fileId).newEdit(deletion);
     }
 
     @Override
     public void files(List<TextFile> textFiles, List<BinaryFile> binaryFiles) {
       Stream.concat(textFiles.stream(), binaryFiles.stream())
           .forEach(this::file);
+    }
+
+    private TextFileClient textFileClient(int fileId) {
+      return subscriptions.get(fileId).textFileClient;
     }
 
     private void file(File file) {
@@ -120,14 +140,14 @@ public class ProjectService {
           TextFile textFile = (TextFile) file;
           textFiles.add(textFile);
           textFileServices.computeIfAbsent(file.getId(),
-              id -> new TextFileService(textFile, clientEndpoint, clients));
+              id -> new TextFileService(textFile));
         } else if (file instanceof BinaryFile) {
           binaryFiles.add(((BinaryFile) file));
         }
       }
       // Notify clients
-      for (ClientEndpoint client : clients) {
-        client.project().files(textFiles, binaryFiles);
+      for (ProjectClient client : clients) {
+        client.files(textFiles, binaryFiles);
       }
     }
 
