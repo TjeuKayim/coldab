02b5ce3b8bd59318d4ea589e6264cf9549e201a1
Tjeu Kayim <tjeu.kayim@gmail.com>
2018-05-21 11:24:17 +0200

    Make fields final

--- shared/src/main/java/com/github/coldab/shared/chat/Chat.java
+++ shared/src/main/java/com/github/coldab/shared/chat/Chat.java
@@ -6,13 +6,8 @@ import java.util.List;
 
 
 public class Chat {
-  private List<ChatMessage> messages;
-  private List<ChatObserver> chatObservers;
-
-  public Chat() {
-    this.messages = new LinkedList<>();
-    this.chatObservers = new LinkedList<>();
-  }
+  private final List<ChatMessage> messages = new LinkedList<>();
+  private final List<ChatObserver> chatObservers = new LinkedList<>();
 
   public void addMessage(ChatMessage message) {
     messages.add(message);
@@ -21,7 +16,6 @@ public class Chat {
   }
 
   public List<ChatMessage> getMessages() {
-    if (messages == null) return null;
     return Collections.unmodifiableList(messages);
   }
 
