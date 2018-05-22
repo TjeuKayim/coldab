c6f81bfebdb531131eac6175fb225d98ecbc02cc
Tjeu Kayim <tjeu.kayim@gmail.com>
2018-05-22 13:22:34 +0200

    Test saving a textFile

--- server/src/test/java/com/github/coldab/server/dal/ProjectStoreTest.java
+++ server/src/test/java/com/github/coldab/server/dal/ProjectStoreTest.java
@@ -2,7 +2,11 @@ package com.github.coldab.server.dal;
 
 import static org.junit.Assert.assertEquals;
 
+import com.github.coldab.shared.account.Account;
+import com.github.coldab.shared.edit.Addition;
 import com.github.coldab.shared.project.Project;
+import com.github.coldab.shared.project.TextFile;
+import java.util.Collections;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,5 +31,17 @@ public class ProjectStoreTest {
         .orElseThrow(IllegalStateException::new);
 
     assertEquals("Hello World", result.getName());
+  }
+
+  @Test
+  public void saveTextFile() {
+    Project project = new Project("Hello World");
+    TextFile textFile = new TextFile(0, "index.html");
+    Account piet = new Account("Piet Hein", "piet@hein.email");
+    textFile.addEdit(new Addition(0, piet, null, "Hello World"));
+    project.getFiles().add(textFile);
+    int id = projects.save(project).getId();
+    project = projects.findById(id).orElseThrow(NullPointerException::new);
+    assertEquals(Collections.singletonList(textFile), project.getFiles());
   }
 }
