bb72bcde91650f7653b48e8f60518dc5739b01c6
Tjeu Kayim <tjeu.kayim@gmail.com>
2018-05-24 12:20:44 +0200

    Add methods remoteAddition and remoteDeletion to TextFileObserver

--- client/src/main/java/com/github/coldab/client/project/TextFileObserver.java
+++ client/src/main/java/com/github/coldab/client/project/TextFileObserver.java
@@ -7,4 +7,24 @@ public interface TextFileObserver {
   void updateAnnotations();
 
   void updateTextFile();
+
+  /**
+   * An remote addition has been created.
+   *
+   * @param start index of the character to insert text after, or -1 if at the start of file
+   * <p>
+   * To insert an "," after "Hello" in "Hello World" to get "Hello, World", start = 4; text = ","
+   * </p>
+   */
+  default void remoteAddition(int start, String text) {
+  }
+
+  /**
+   * An remote deletion has been created.
+   *
+   * @param start the first character to delete, inclusive
+   * @param length the number of characters to delete
+   */
+  default void remoteDeletion(int start, int length) {
+  }
 }
