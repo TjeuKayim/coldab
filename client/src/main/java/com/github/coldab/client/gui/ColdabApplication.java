1dd36889c7543dd71640ab0ed3853114189a6cd3
Tim <tim.krijnen@student.fontys.nl>
2018-06-04 10:45:03 +0200

    Authentication as first stage

--- client/src/main/java/com/github/coldab/client/gui/ColdabApplication.java
+++ client/src/main/java/com/github/coldab/client/gui/ColdabApplication.java
@@ -11,17 +11,32 @@ import javafx.stage.Stage;
 public class ColdabApplication extends Application {
 
   private Stage projectChooserStage;
+  private Stage authenticationStage;
 
   @Override
   public void start(Stage primaryStage) throws IOException {
-    this.projectChooserStage = primaryStage;
-    FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/projectChooser.fxml"));
-    loader.setControllerFactory(c -> new ProjectChooserController(this::startEditor));
+    this.authenticationStage = primaryStage;
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/authentication.fxml"));
+    loader.setControllerFactory(c -> new AuthenticationController());
     Parent root = loader.load();
-    projectChooserStage.setTitle("Coldab text");
+    authenticationStage.setTitle("Coldab Login");
     Scene scene = new Scene(root);
-    projectChooserStage.setScene(scene);
-    projectChooserStage.show();
+    authenticationStage.setScene(scene);
+    authenticationStage.show();
+
+
+//    this.projectChooserStage = primaryStage;
+//    FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/projectChooser.fxml"));
+//    loader.setControllerFactory(c -> new ProjectChooserController(this::startEditor));
+//    Parent root = loader.load();
+//    projectChooserStage.setTitle("Coldab text");
+//    Scene scene = new Scene(root);
+//    projectChooserStage.setScene(scene);
+//    projectChooserStage.show();
+  }
+
+  private void startProjectChooser(){
+
   }
 
   private void startEditor(Project project) {
