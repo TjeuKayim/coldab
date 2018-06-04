3d534ce0e9ead092a58e0de9cd289fd48a0e1732
Tim <tim.krijnen@student.fontys.nl>
2018-06-04 11:42:50 +0200

    Event handler on hyperlink, authentication GUI edits

--- client/src/main/java/com/github/coldab/client/gui/AuthenticationController.java
+++ client/src/main/java/com/github/coldab/client/gui/AuthenticationController.java
@@ -2,9 +2,13 @@ package com.github.coldab.client.gui;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class AuthenticationController implements Initializable {
@@ -14,10 +18,17 @@ public class AuthenticationController implements Initializable {
   @FXML
   private TextField password;
   @FXML
+  private Hyperlink register;
+  @FXML
   private Button login;
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
-
+    register.setOnAction(this::registerHyperlinkClicked);
   }
+
+  private void registerHyperlinkClicked(ActionEvent actionEvent) {
+    System.out.println(actionEvent.toString());
+  }
+
 }
