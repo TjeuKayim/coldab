c7fab20e326b55092e6d1ca2db40117726125bcd
Tim <tim.krijnen@student.fontys.nl>
2018-06-04 13:14:52 +0200

    Open new register screen, register GUI edit

--- client/src/main/java/com/github/coldab/client/gui/AuthenticationController.java
+++ client/src/main/java/com/github/coldab/client/gui/AuthenticationController.java
@@ -1,13 +1,18 @@
 package com.github.coldab.client.gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class AuthenticationController implements Initializable {
 
@@ -20,6 +25,8 @@ public class AuthenticationController implements Initializable {
   @FXML
   private Button login;
 
+  private Stage registerStage;
+
   @Override
   public void initialize(URL location, ResourceBundle resources) {
     register.setOnAction(this::registerHyperlinkClicked);
@@ -27,6 +34,20 @@ public class AuthenticationController implements Initializable {
 
   private void registerHyperlinkClicked(ActionEvent actionEvent) {
     //System.out.println(actionEvent.toString());
+
+    Stage stage = new Stage();
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/register.fxml"));
+    loader.setControllerFactory(c -> new RegisterController());
+    Parent root = null;
+    try {
+      root = loader.load();
+    } catch (IOException e) {
+      throw new IllegalStateException("Could not load FXML");
+    }
+    stage.setTitle(String.format("Register Coldab Account"));
+    Scene scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
   }
 
 }
