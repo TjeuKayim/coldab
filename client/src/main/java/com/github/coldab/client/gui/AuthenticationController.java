8d455ccc92834478a61451345999243d2d08c3fa
Tim <tim.krijnen@student.fontys.nl>
2018-06-04 11:09:17 +0200

    Authentication GUI

--- client/src/main/java/com/github/coldab/client/gui/AuthenticationController.java
+++ client/src/main/java/com/github/coldab/client/gui/AuthenticationController.java
@@ -2,9 +2,19 @@ package com.github.coldab.client.gui;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class AuthenticationController implements Initializable {
+
+  @FXML
+  private TextField username;
+  @FXML
+  private TextField password;
+  @FXML
+  private Button login;
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
