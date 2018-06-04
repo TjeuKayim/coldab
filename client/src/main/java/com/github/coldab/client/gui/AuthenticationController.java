845b333eb34a0e7480faddb59ef1bfe75d8aeda8
Tim <tim.krijnen@student.fontys.nl>
2018-06-04 10:33:05 +0200

    Authentication controller

---
+++ client/src/main/java/com/github/coldab/client/gui/AuthenticationController.java
@@ -0,0 +1,13 @@
+package com.github.coldab.client.gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class AuthenticationController implements Initializable {
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    
+  }
+}
