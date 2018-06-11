47a470e581ec9804040b800f6b4fc74fd467876f
victorvanleeuwen <v.vn.leeuwen@gmail.com>
2018-06-11 10:44:20 +0200

    validatepassword test finnished

--- shared/src/test/java/com/github/coldab/shared/Account/AccountTest.java
+++ shared/src/test/java/com/github/coldab/shared/Account/AccountTest.java
@@ -1,21 +1,30 @@
 package com.github.coldab.shared.Account;
 
 import com.github.coldab.shared.account.Account;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Before;
 import org.junit.Test;
 
 public class AccountTest {
 
   private Account account;
+  private String correct;
+  private String incorrect;
 
   @Before
   public void setup(){
-    account = new Account("TestUser","Test@user.com");
+    account = new Account("TestUser","Test@user.com","Strongpassword");
+    correct = "Strongpassword";
+    incorrect = "weakpassword";
   }
 
   @Test
   public void validate(){
-
+    assertTrue(account.validatePassword(correct));
+    assertFalse(account.validatePassword(incorrect));
   }
 }
 
