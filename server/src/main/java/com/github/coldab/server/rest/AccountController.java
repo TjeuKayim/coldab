95091c0d53a2e6b0dae0e4bceb2c6eee039e1cfb
Tjeu Kayim <tjeu.kayim@gmail.com>
2018-06-11 11:10:14 +0200

    Implement AccountController server side

--- server/src/main/java/com/github/coldab/server/rest/AccountController.java
+++ server/src/main/java/com/github/coldab/server/rest/AccountController.java
@@ -1,7 +1,9 @@
 package com.github.coldab.server.rest;
 
 import com.github.coldab.server.dal.AccountStore;
+import com.github.coldab.server.services.LoginSessionManager;
 import com.github.coldab.shared.account.Account;
+import com.github.coldab.shared.rest.Credentials;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/account/", method = RequestMethod.POST)
 public class AccountController {
   private final AccountStore accountStore;
+  private final LoginSessionManager sessionManager;
 
-  public AccountController(AccountStore accountStore) {
+  public AccountController(AccountStore accountStore,
+      LoginSessionManager sessionManager) {
     this.accountStore = accountStore;
+    this.sessionManager = sessionManager;
   }
 
   /**
@@ -22,23 +27,31 @@ public class AccountController {
    * @return Account met SID
    */
   @PostMapping("login")
-  public Account login(String email, String password) {
-    Account account = accountStore.findAccountByemail(email);
+  public Account login(Credentials credentials) {
+    Account account = accountStore.findAccountByemail(credentials.getEmail());
     if (account != null) {
-      //TODO: check if password is correct
-
+      if (account.validatePassword(credentials.getPassword())) {
+        // Valid login
+        sessionManager.login(account);
+        return account;
+      } else {
+        // Invalid login
+        return null;
+      }
     }
-
     return null;
   }
 
   @PostMapping("register")
-  public Account register(String email, String password) {
-    return null;
+  public Account register(Credentials credentials) {
+    Account account = new Account(null, credentials.getEmail(), credentials.getPassword());
+    accountStore.save(account);
+    sessionManager.login(account);
+    return account;
   }
 
   @PostMapping("logout")
   public void logout(String sessionId) {
-
+    sessionManager.logout(sessionId);
   }
 }
