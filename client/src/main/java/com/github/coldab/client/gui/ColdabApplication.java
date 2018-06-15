c6169dbb08b5440f86994323dfff4a2be4a2febf
Tjeu Kayim <tjeu.kayim@gmail.com>
2018-06-15 14:55:24 +0200

    Do todo: Make use of parameter account instead of hardcoding it

--- client/src/main/java/com/github/coldab/client/gui/ColdabApplication.java
+++ client/src/main/java/com/github/coldab/client/gui/ColdabApplication.java
@@ -57,9 +57,7 @@ public class ColdabApplication extends Application {
     projectChooserStage.show();
   }
 
-  private void startEditor(Project project, Account todo) {
-    //todo: Use account parameter
-    Account account = project.getAdmins().iterator().next();
+  private void startEditor(Project project, Account account) {
     projectChooserStage.hide();
     EditorController controller = new EditorController(project, account);
     FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/editor.fxml"));
