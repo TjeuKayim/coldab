336cafb8f100a73b3680c7a194c8903bdd6ee11d
Tjeu Kayim <tjeu.kayim@gmail.com>
2018-06-24 16:04:38 +0200

    Reproduce issue #20

--- server/src/test/java/com/github/coldab/server/dal/ProjectStoreTest.java
+++ server/src/test/java/com/github/coldab/server/dal/ProjectStoreTest.java
@@ -12,7 +12,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -20,12 +19,12 @@ import org.springframework.test.context.junit4.SpringRunner;
  * Integration test for database.
  */
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = Main.class, webEnvironment = WebEnvironment.RANDOM_PORT)
+@SpringBootTest(classes = Main.class)
 @ActiveProfiles(profiles = "dev")
 public class ProjectStoreTest {
 
   @Autowired
-  private ProjectStore projects;
+  private ProjectStore projectStore;
   @Autowired
   private AccountStore accountStore;
   @Autowired
@@ -35,9 +34,9 @@ public class ProjectStoreTest {
   public void getById() {
     Project project = new Project("Hello World");
 
-    int id = projects.save(project).getId();
+    int id = projectStore.save(project).getId();
 
-    Project result = projects.findById(id)
+    Project result = projectStore.findById(id)
         .orElseThrow(IllegalStateException::new);
 
     assertEquals("Hello World", result.getName());
@@ -46,22 +45,43 @@ public class ProjectStoreTest {
   @Test
   public void saveTextFile() {
     // Create account
-    Account piet = new Account("Piet Hein8", "piet8@hein.email", "1234");
-    accountStore.save(piet);
+    Account account = createAccount("saveTextFile");
+    accountStore.save(account);
     // Create textFile with edit
     TextFile textFile = new TextFile(0, "index.html");
-    textFile.addEdit(new Addition(0, piet, null, "Hello World"));
+    textFile.addEdit(new Addition(0, account, null, "Hello World"));
     fileStore.save(textFile);
     // Create project, and add textFle
     Project project = new Project("Hello World");
     project.getFiles().add(textFile);
-    int id = projects.save(project).getId();
+    int id = projectStore.save(project).getId();
     // After saving everything, get it back and assert if equal
-    project = projects.findById(id).orElseThrow(NullPointerException::new);
+    project = projectStore.findById(id).orElseThrow(NullPointerException::new);
     assertEquals(1, project.getFiles().size());
     TextFile actual = (TextFile) project.getFiles().iterator().next();
     assertEquals(textFile.getId(), actual.getId());
     assertArrayEquals(textFile.getPath(), actual.getPath());
     assertEquals(textFile.getEdits().get(0), actual.getEdits().get(0));
+  }
+
+  @Test
+  public void reproduce_issue_20() {
+    // Bob creates a project
+    Account bob = createAccount("Bob");
+    Project project = projectStore.save(new Project("MyProject"));
+    project.getAdmins().add(bob);
+    // Add a file
+    TextFile file = new TextFile(0, "hello.world");
+    fileStore.save(file);
+    project.getFiles().add(file);
+    projectStore.save(project);
+    // Share with alice
+    Account alice = createAccount("Alice");
+    project.getAdmins().add(alice);
+    projectStore.save(project);
+  }
+
+  private Account createAccount(String name) {
+    return accountStore.save(new Account(name, name, name));
   }
 }
--- server/src/test/java/com/github/coldab/server/services/ShareTest.java
+++ server/src/test/java/com/github/coldab/server/services/ShareTest.java
@@ -3,6 +3,7 @@ package com.github.coldab.server.services;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
+import com.github.coldab.server.Main;
 import com.github.coldab.server.dal.AccountStore;
 import com.github.coldab.server.dal.FileStore;
 import com.github.coldab.server.dal.ProjectStore;
@@ -15,14 +16,16 @@ import com.github.coldab.shared.ws.ProjectServer;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * Integration test for sharing a project.
  */
 @RunWith(SpringRunner.class)
-@DataJpaTest
+@SpringBootTest(classes = Main.class)
+@ActiveProfiles(profiles = "dev")
 public class ShareTest {
 
   @Autowired
